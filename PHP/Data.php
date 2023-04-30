2ae7c0516ae21c5583314bc05ea94341ce6820e5
Williamsulasman <williamsulasman17@gmail.com>
2023-05-01 01:03:17 +0700

    UTS Server and Network Administration

---
+++ PHP/Data.php
@@ -0,0 +1,27 @@
+<?php
+$servername = "172.20.0.2";
+$username = "root";
+$password = "pass12345";
+$dbname = "Redlock";
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+if(!$conn){
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql = "SELECT COUNT(*) AS SUM FROM users";
+
+$result = mysqli_query($conn, $sql);
+
+if(mysqli_num_rows($result) > 0){
+    $row = mysqli_fetch_array($result)
+    echo "Jumlah Data Adalah " . $row["SUM"];
+} 
+
+else{
+    echo "0 results";
+}
+
+mysqli_close($conn);
+?>
